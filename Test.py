637959fd7c4bedde852f376e39dc2755f01d058c
Mickhail07 <78mickhailch@gmail.com>
2026-04-01 10:03:10 +0500

    Solved error.

--- Test.py
+++ Test.py
@@ -1,4 +1,26 @@
-from datetime import datetime, timedelta
-a = datetime.now()
-b = a - timedelta(days=5)
-print(b)
+with open("in.txt") as f:
+    a = f.readline()
+    b = f.readline()
+a = a.strip().split(",")
+b = b.strip().split(",")
+e = list(zip(a, b))
+print(*e)
+c = dict(zip(a, b))
+d = list()
+for i in c:
+    t = int(i)
+    d.append(t * int(c.get(i)))
+print(*d)
+f = list()
+g = list()
+for i, k in e:
+    f.append(int(i))
+    g.append(int(k))
+h = 1
+for i in f:
+    h = h * i
+print(h)
+h = 1
+for i in g:
+    h = h * i
+print(h)
